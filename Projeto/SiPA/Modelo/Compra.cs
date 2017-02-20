9ff02677a108868bd78e3f4981df11d523d4b953
Iury Lamonie <iury.lamonie@hotmail.com>
2017-02-20 17:49:56 -0300

    Compra e ItemCompra

--- Projeto/SiPA/Modelo/Compra.cs
+++ Projeto/SiPA/Modelo/Compra.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,47 @@ namespace SiPA.Modelo
 {
     public class Compra
     {
+        public static void Incluir(SiPA.Compra compra)
+        {
+            BancoDataContext dc = new BancoDataContext();
+            dc.Compras.InsertOnSubmit(compra);
+            dc.SubmitChanges();
+        }
+
+        public static void Alterar(SiPA.Compra compra)
+        {
+            BancoDataContext dc = new BancoDataContext();
+            SiPA.Compra r = (from c in dc.Compras
+                             where c.id == compra.id
+                             select c).Single();
+            r = compra;
+            dc.SubmitChanges();
+        }
+
+        public static void Excluir(int id)
+        {
+            BancoDataContext dc = new BancoDataContext();
+            SiPA.Compra compra = (from c in dc.Compras where c.id == id select c).Single();
+            dc.Compras.DeleteOnSubmit(compra);
+            dc.SubmitChanges();
+        }
+
+        [DataObjectMethod(DataObjectMethodType.Select)]
+        public static List<SiPA.Compra> Listar()
+        {
+            BancoDataContext dc = new BancoDataContext();
+            List<SiPA.Compra> compras = (from com in dc.Compras select com).ToList();
+            return compras;
+        }
+
+        [DataObjectMethod(DataObjectMethodType.Select)]
+        public static List<SiPA.Compra> Filtro(string fornecedor, DateTime periodo)
+        {
+            BancoDataContext dc = new BancoDataContext();
+            List<SiPA.Compra> compras = (from com in dc.Compras
+                                         where com.Fornecedor.nome == fornecedor && com.dataCompra == periodo
+                                         select com).ToList();
+            return compras;
+        }
     }
 }
--- Projeto/SiPA/Modelo/ItemCompra.cs
+++ Projeto/SiPA/Modelo/ItemCompra.cs
@@ -7,5 +7,20 @@ namespace SiPA.Modelo
 {
     public class ItemCompra
     {
+        public static void Incluir(SiPA.ItemCompra itemCompra)
+        {
+            BancoDataContext dc = new BancoDataContext();
+            dc.ItemCompras.InsertOnSubmit(itemCompra);
+            dc.SubmitChanges();
+        }
+
+        public static List<SiPA.ItemCompra> Listar(int id)
+        {
+            BancoDataContext dc = new BancoDataContext();
+            List<SiPA.ItemCompra> ic = (from it in dc.ItemCompras
+                                        where it.compra_id == id
+                                        select it).ToList();
+            return ic;
+        }
     }
 }
